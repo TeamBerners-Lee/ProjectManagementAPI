d68b00eeef8fd83879a4e693751c48404df1bbca
Annie <anne.bourgeois1@gmail.com>
2015-03-17 12:01:36 -0400

    Changes to the avatar default url for users

--- db/migrate/20150312154242_create_users.rb
+++ db/migrate/20150312154242_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
       t.string :title
       t.string :token
-      t.string :avatar
+      t.string :avatar, default: 'https://s3.amazonaws.com/project-management-bucket/missing_avatar.png'
     end
   end
 end
