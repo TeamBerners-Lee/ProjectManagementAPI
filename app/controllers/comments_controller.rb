f70fbbbe74d41ab7eb2600b6b068ad7f23c6e105
Annie <anne.bourgeois1@gmail.com>
2015-03-18 20:21:28 -0400

    Added user_id in comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,7 +33,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body, :username, :task_id)
+    params.require(:comment).permit(:body, :username, :task_id, :user_id)
   end
 end
 
